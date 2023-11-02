a764ee7ceb143808c8b88bab880e99019d0a06fa
ewillwin <jw3418@naver.com>
2023-11-03 04:36:41 +0900

    Create ItemController.java

---
+++ src/main/java/com/example/myapp/controller/ItemController.java
@@ -0,0 +1,20 @@
+package com.example.myapp.controller;
+
+import com.example.myapp.model.Item;
+import com.example.myapp.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/items")
+public class ItemController {
+    private final ItemService itemService;
+
+    @Autowired
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+}
